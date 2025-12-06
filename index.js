d513e80f06d59c507b159d39520658d18480c5ec
Elbek <elbekraximberganov011@gmail.com>
2025-12-06 17:17:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,384 @@
+// let matn ="Mirzobek";
+// let teskari = matn.split('').reverse().join('');
+// console.log(matn);
+// console.log(teskari);
+
+
+
+
+// let x=10;
+// console.log(x,typeof(x));
+// let y="men";
+// y="sen"
+// console.log(y,typeof(y));
+// let z;
+// console.log(z,typeof(z));
+// let a = true;
+// // let a =false;
+// console.log(a,typeof(a));
+// let b=[1,2,3,4,5,6,7,8,9];
+// console.log(b,typeof(b));
+// let c={
+//    name:"Mirzobek",
+//    age:14
+// }
+// console.log(c,typeof(c));
+
+
+
+// // alert("bu tashqi link")
+// // let ism="Mirzobek"
+// // let fam=("Rozmetov")
+// // let date=("09.04.2011")
+// // console.log(ism);
+// // console.log(fam);
+// // // console.log(date);
+// // 14.10.2025
+// let x=20
+// let y=4
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(x**y);
+// 
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x>=y);
+// console.log(x<=y);
+// console.log(x==y);
+// console.log(x!=y);
+// // Mantiqiy operatorlar
+// let a=5; b=10; c=15; d=20;
+// console.log(a>b && c>d)
+// console.log(a<b && c<d)
+// console.log(a>b && c<d)
+// console.log(a>b || c>d)
+// console.log(a<b || c<d)
+// console.log(!(a<b || c<d));
+// Tayinlash operatori
+// let z=15
+// console.log(z+=5)
+// console.log(z=z+5)
+// console.log(z=z-5)
+// console.log(z=z-5)
+// console.log(z*=2)
+// console.log(z=z*2)
+// console.log(z=z/5)
+// console.log(z=z/2)
+// console.log(z%=4)
+// console.log(z**=5)
+// let yosh = 14;
+// let yosh2 = 19;
+// let natija =yosh2>=18 && !(yosh>=18) ? console.log("Ha") :console.log("Yo'q")
+// if(yosh>=18){
+//    console.log("Balog'atga yetgan");
+// }
+// else{
+//    console.log("Balog'atga yetmagan")
+// }
+
+// let ball=prompt('ball');
+// if(ball>=86){
+//    console.log("A'lo")
+// }
+// else if(ball>=70){
+//    console.log("Yaxwi")
+// }
+// else if(ball>=50){
+//    console.log("Qoniqarli")
+// }
+// else{
+//    console.log("Qoniqarsiz ")
+// }
+// let son1 =200000
+// let son2 =1000
+
+// // console.log(("Son%2===0") ? 'Juft' : "Tog" )
+// // console.log(son>0 ? 'Ijobiy' : (son<0) ? 'Manfiy' : 'Nol');
+// if(son1){
+// console.log('son1 katta')
+// }
+// else if(son2){
+//    console.log('son2 katta')
+// }
+// else{
+//    console.log('teng')
+// }
+
+// let saot=prompt('soat:');
+
+// if(saot>=6 && saot<12){
+//    console.log("Xayrli tong")
+// }
+// else if(saot>=12 && saot<18){
+//    console.log("Xayrli kun")
+// }
+// else  if(saot>=18 && saot<22) 
+// {
+//    console.log("Xayrli kech")
+// }
+// else{
+//    console.log("Tungi Salom")
+// }
+
+// let fln=prompt('login:');
+// let fpl=prompt('parol:');
+// if(In=== fln && pl===fpl){
+//    alert(
+//       'Sahifamizga hush kelibsz')
+// } else{
+//    alert('Parol yoki login xato!')
+// }
+// let matn ="Mirzobek";
+// let teskari = matn.split('').reverse().join('');
+// // console.log(matn);
+// // console.log(teskari);
+// if (matn === teskari){
+//    console.log(`${matn}`)
+// }
+
+
+
+// let kun = prompt('oy kiriting:');
+
+// switch (kun) {
+//    case '1':
+//    case '12':
+//    case '2':
+//       console.log('Qish')
+//       break;
+//    case '3':
+//    case '4':
+//    case '5':
+//       console.log('Bahor')
+//       break;
+//    case '6':
+//    case '7':
+//    case '8':
+//       console.log('Yoz')
+//       break;
+
+//    case '9':
+//    case '10':
+//    case '11':
+//       console.log('Kuz')
+//       break;
+//    default:
+//       console.log('bunday fasl yoq');
+
+// }
+
+// let son = prompt('son')
+// console.log(typeof son);
+// let son1= Number(son);
+// console.log(typeof son1);
+
+// let son = prompt('son:');
+// console.log(typeof son);
+// let sonint=parseInt(son);
+// console.log(typeof sonint);
+
+
+
+
+
+
+
+// let kun = prompt('tilni kiriting:(namuna:us,eng,ru)')
+// switch (kun) {
+//    case 'us':
+//       alert('Salom')
+//       break;
+//        case 'ru':
+//       alert('Привет')
+//       break;
+//         case '':
+//       alert('hello')
+//       break;
+// }
+
+
+
+
+
+
+// let son1=Number(prompt('son1') )
+// let son2=Number(prompt('son2') )
+// let amal=prompt('Amal')
+// switch (amal) {
+//    case '+':
+//       console.log(son1 + son2)
+//       break;
+//          case '-':
+//       console.log(son1 - son2)
+//       break;
+//          case '/':
+//       console.log(son1 / son2)
+//       break;
+//          case '*':
+//       console.log(son1 * son2)
+//       break;
+// }
+
+
+
+
+// 1-NOYABR
+
+
+
+
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// for(let i=1; i<=100000; i+=3){
+// console.log(i)
+// }
+// let n=Number(prompt ("Son" ))
+// for(let i=1; i<=n; i++){
+//    if(i%3==0&&i%5==0){
+//       console.log(i);
+//    }
+// }
+// for(let i=1; i<=10; i++){
+//    for( let j=1; j<=10; j++){
+//       console.log(`${i} X ${j} = ${i*j}`);
+//    }
+// }
+// let n=Number(prompt("son:"));
+// let summa=0; sanoqchi=0;
+// for(let i=1; i<=n; i++){
+//    summa=summa+i
+//    sanoqchi++;
+//    console.log('sanoqchi = '+sanoqchi)
+//    console.log('i- '+i);
+//    console.log(summa)
+// }
+// console.log("o'rta arifmetik = "+summa/sanoqchi);
+
+
+
+
+// 11 11 massivlar
+
+// console.log(guruh1);
+// document.getElementById('demo').innerHTML=guruh1;
+// console.log(guruh2[0])
+// document.getElementById('demo2').innerHTML=
+// `guruh2 ichidagi a'zolar soni = ${guruh2.length} ta`
+// guruh2[2]='Nodirbek'
+// guruh2[5]='Umrbek'
+// console.log(guruh2)
+// const  sinf=guruh1.concat(guruh2)
+// console.log(sinf.concat('Joshqin','Sarver'))
+
+// console.log(guruh1.toString())
+// console.log(guruh1.join(' va '))
+// console.log(guruh2)
+// console.log(guruh2[4]);
+// console.log(guruh2.at(-2));
+// let meva2=['Olma','Gilos','Olcha','Nok','Uzum','Behi']
+// console.log(meva)
+// console.log(meva.pop());
+// console.log(meva.pop());
+// console.log(meva.pop());
+// console.log(meva.shift());
+// console.log(meva.shift());
+
+// console.log(meva.push('Anor'))
+// console.log(meva.unshift('Orik'))
+
+// let sonlar = [[1,2],[3,4],[5,6]]
+// console.log(sonlar)
+// console.log(sonlar.flat())
+
+// let sons=[1,2,[3,4,[5,6,[7,8,[9,10]]]]]
+// console.log(sons)
+// console.log(sons.flat(4))
+// console.log(meva2)
+// console.log(meva2.splice(2,3));
+// console.log(meva2)
+// let meva3=meva2.toSpliced(2,3)
+// console.log(meva3);
+// console.log(meva2);
+// let meva4=meva2.slice(2,5)
+// console.log(meva4)
+// let sinf2=guruh1.concat(guruh2)
+// console.log(sinf2)
+
+// console.log(sinf2.indexOf('Elbek',5));
+// console.log(sinf2.indexOf('Elbek',5))
+// let son2=[10,60,30,40,20,50];
+// console.log(son2.find((a) => (a+30)%9==1))
+// console.log(son2.findIndex((b) => (b-30)/3== 10))
+
+// 18.11.2025
+// let meva2=['Olma','gilos','Olcha','nok','Uzum',1,'Ы','апелсин', 'Behi',  'anor','Banana','dovcha',];
+// let alfa=['A', 'b', 'C','d','G', 1,2,3, 'i',]
+// console.log(meva2.sort())
+// console.log(meva2.sort((a,b) => a.localeCompare(b)))
+
+
+// let sonlar = [12,5,8,130,44,27,33,90]
+// console.log(sonlar.filter((a) => a % 2== 0 ).sort((a,b) => b-a))
+// let matnlar =" JavaScript dasturlash tilli juda qiziqarli va kuchli"
+// console.log(matn)
+// console.log(matn.split())
+// console.log(matn.split(""))
+// console.log(matn.split( " ").filter(a=> a.length >=5).sort((a,b) => a.length-b.length))
+// let mevalar=['Olma','Gilos','Shaftoli','Uzum']
+// console.log(mevalar)
+// mevalar.forEach((qiymat, index) => {
+//    console.log(` ${index+1}) ${qiymat} ----forEach`)
+// })
+
+// let mson=[1,2,3,4,5];
+// console.log(mson)
+// console.log(mson.map(a=> a**3))
+// mson.forEach((a) =>{
+//     console.log()
+// }
+// )
+// let oquvchilar =[
+//     {ism: "Ali",baho:95},
+//     {ism: "Vali",baho:78},
+//     {ism: "Salim",baho:88},
+//     {ism: "Zokir",baho:65},
+// ];
+// console.log(oquvchilar)
+// oquvchilar.forEach(`${index+1}) - ${oquvchilar.ism} - ${oquvchilar.baho} `)
+// let ishchilar = [
+//     { ism: "Ali", maosh: 500 },
+//     { ism: "Vali", maosh: 700 },
+//     { ism: "Salim", maosh: 900 },
+//     { ism: "Zokir", maosh: 1200 },
+// ];
+// console.log(ishchilar)
+// ishchilar.forEach((a, b) => {
+//     console.log(`${b + 1} ${a.nmi} - ${a.maosh + a.maosh / 10}som`)
+// })
+// let mahsulotlar = [
+//     { nomi: "Noutbuk", narx: 2000 },
+//     { nomi: "Telefon", narx: 1500 },
+//     { nomi: "Planshet", narx: 1000 },
+//     { nomi: "Quloqchin", narx: 200 }
+// ];
+
+// mahsulotlar.forEach(mahsulot => {
+//     let chegirmaNarx = mahsulot.narx * 0.8;
+//     let Narx = mahsulot.narx ;
+//     console.log(`${mahsulot.nomi} asl narx $${Narx}  - chegirma bilan: $${chegirmaNarx}`);
+// })
+let odamlar = [
+    { ism: "Aziz", familiya: "Yusupov" },
+    { ism: "Mirzobek", familiya: "Ro'zmetov" },
+    { ism: "Salim", familiya: "Baxodirov" },
+    { ism: "Elbek", familiya: "Raximberganov" },
+];
+let toliqnom = odamlar.map(odam => `${odam.ism} ${odam.familiya}`);
+console.log(toliqnom);
